22e4b233508ebcbc9c73f313effccc136bead1f0
Mike Mackintosh <m@splug.io>
2014-05-21 11:17:58 -0400

    initial commit

---
+++ config.ru
@@ -0,0 +1,3 @@
+require_relative 'transmission'
+
+run Transmission.new
---
+++ transmission.rb
@@ -0,0 +1,7 @@
+require 'rubygems' if RUBY_VERSION < '1.9'
+require 'sinatra/base'
+
+class Transmission < Sinatra::Base
+  get '/' do
+  end
+end
