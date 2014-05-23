97f24d4dfe31059593f21a8c858ac7ebe66d9175
Mike Mackintosh <m@splug.io>
2014-05-23 15:56:04 -0400

    removed helper

--- helpers/system.rb
+++
@@ -1,7 +0,0 @@
-module Transmission
-  class SystemHelper
-    def create_ssh_key( user )
-      %x[ssh-keygen -t rsa -b 4093 -q -C "Key for user: #{user}" -f #{@config.app_path}/keys/#{user}_rsa ]
-    end
-  end
-end
