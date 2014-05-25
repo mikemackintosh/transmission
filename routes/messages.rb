fcbcdecdd68c01cc56e4e7ce80b9e400af72b233
Mike Mackintosh <m@splug.io>
2014-05-25 15:07:33 -0400

    added message auth and message status udate

--- routes/messages.rb
+++ routes/messages.rb
@@ -19,9 +19,34 @@ module Transmission
       erb :messages
     end
 
-    get '/message/:name' do
-      #private_key = OpenSSL::PKey::RSA.new(File.read(target_private_key), passphrase)
-      #original_key = private_key.private_decrypt secure
+    get '/message/:msgid' do
+      @uri = "#{request.path}?#{request.query_string}"
+      erb :message_auth
+    end
+
+    post '/message/:msgid' do
+      message = {}
+      Dir["#{settings.app_path}/messages/#{session['username']}*"].each do |messages|
+        msg = /(.*)_(.*)_(.*)/.match(messages).to_s.split("_")
+        if params[:msgid] == Digest::MD5.hexdigest(msg[0])
+          message[:from] = msg[1]
+          message[:date] = msg[2]
+          secure_message = File.read(messages).to_s[0..-3]
+          private_key = OpenSSL::PKey::RSA.new(File.read("#{settings.app_path}/keys/#{session['username']}_rsa"), params[:password])
+          content = private_key.private_decrypt Base64.decode64(secure_message)
+
+          # Set Read Flag
+          File.open(messages, 'w') { |file| 
+            file.write("#{secure_message} ,1") 
+          }
+
+          message[:content] = content
+          @message = message          
+        end
+      end
+
+      erb :message
+          
     end
 
     get '/send' do
